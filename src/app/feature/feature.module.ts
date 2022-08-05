19ae87bee9ae66ca62a46765766edb23800975de
1727290395 <jjb.simbana@yavirac.edu.ec>
2022-08-04 21:39:05 -0500

    combobox e

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -6,6 +6,7 @@ import { CategoryFormComponent } from './category/form/category.form.component';
 import { FormsModule } from '@angular/forms';
 import { CategoryListComponent } from './category/list/category-list.component';
 import { CategoryToolbarComponent } from './category/toolbar/category-toolbar.component';
+import { ActivityComboboxComponent } from './activity/activity-combobox.component';
 import { AuthoritySearchComponent } from './authority/authority-search/authority-search.component';
 import { ActivityFormComponent } from './activity/form/activity.form.component';
 import { ActivityListComponent } from './activity/list/activity-list.component';
@@ -17,6 +18,7 @@ import { ActivityToolbarComponent } from './activity/toolbar/activity-toolbar.co
     CategoryFormComponent,
     CategoryListComponent,
     CategoryToolbarComponent,
+    ActivityComboboxComponent,
     ActivityFormComponent,
     AuthoritySearchComponent,
     ActivityListComponent,
